ef8ce032e4ca9d255af4ce2021f02356f485958f
Jon Phipps <jphipps@madcreek.com>
2012-06-16 16:09:54 -0400

    Alternative formats

---
+++ mappings_schema_org.php
@@ -0,0 +1,376 @@
+array (
+  'http://dublincore.org/mappings/schema.org/' => 
+  array (
+    'http://www.w3.org/1999/02/22-rdf-syntax-ns#type' => 
+    array (
+      0 => 
+      array (
+        'value' => 'http://www.w3.org/2002/07/owl#Ontology',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:Organization' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subClassOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:Agent',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:Person' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subClassOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:Agent',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:Place' => 
+  array (
+    'http://www.w3.org/2002/07/owl#equivalentClass' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:Location',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:Event' => 
+  array (
+    'http://www.w3.org/2002/07/owl#equivalentClass' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dctype:Event',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:Language' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subClassOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:LinguisticSystem',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'dct:BibliographicResource' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subClassOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'schema:CreativeWork',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'dct:title' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'schema:name',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:description' => 
+  array (
+    'http://www.w3.org/2002/07/owl#equivalentProperty' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:description',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:caption' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:description',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:author' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:creator',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:about' => 
+  array (
+    'http://www.w3.org/2002/07/owl#equivalentProperty' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:subject',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:publisher' => 
+  array (
+    'http://www.w3.org/2002/07/owl#equivalentProperty' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:publisher',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:datePublished' => 
+  array (
+    'http://www.w3.org/2002/07/owl#equivalentProperty' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:issued',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:inLanguage' => 
+  array (
+    'http://www.w3.org/2002/07/owl#equivalentProperty' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dc:language',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:encodings' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:format',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:duration' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:extent',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:keywords' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dc:subject',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:url' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:identifier',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:contentLocation' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:coverage',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:subEvents' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:hasPart',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:superEvent' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:isPartOf',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:contributor' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:contributor',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:illustrator' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:contributor',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:actors' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:contributor',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:director' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:contributor',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:musicBy' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:contributor',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:producer' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:creator',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:productionCompany' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:creator',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:productId' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:identifier',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:commentTime' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:date',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:birthDate' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:date',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:dateModified' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:modified',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+  'schema:mentions' => 
+  array (
+    'http://www.w3.org/2000/01/rdf-schema#subPropertyOf' => 
+    array (
+      0 => 
+      array (
+        'value' => 'dct:related',
+        'type' => 'uri',
+      ),
+    ),
+  ),
+)
